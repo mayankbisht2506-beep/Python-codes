dd6297a597cd953ef1f60b9d094769c0c796aca9
mayankbisht2506-beep <mayankbisht2506@gmail.com>
2026-02-01 22:54:06 +0530

    Update MCMC_stress.py

--- MCMC_stress.py
+++ MCMC_stress.py
@@ -78,7 +78,7 @@ def hubble_model(z, params):
 # This function models the "Effective Expansion History" H_eff(z) required to
 # match the net luminosity distance D_L.
 #
-# In the analytic theory (manuscript Eq. 70-75):
+# In the analytic theory:
 #   1. Gravity Boost (G_early > G0) INCREASES H(z) -> Brightens SNe.
 #   2. Viscous Damping (Opacity/Drag) DIMS SNe.
 #
